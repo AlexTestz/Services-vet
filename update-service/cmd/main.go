b3849afc09afac0f88521d021d1db9015e2c8762
AlexMorales <admoralesp@uce.edu.ec>
2025-07-06 18:50:30 -0500

    changes

--- update-service/cmd/main.go
+++ update-service/cmd/main.go
@@ -5,6 +5,7 @@ import (
 	"github.com/AlexTestz/update-service/database"
 	"github.com/AlexTestz/update-service/routes"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {
@@ -21,4 +22,11 @@ func main() {
 	routes.SetupRoutes(app)
 
 	app.Listen(":" + config.GetEnv("PORT"))
+
+	app.Use(cors.New(cors.Config{
+  AllowOrigins: "*",  // Permite solicitudes desde cualquier origen
+  AllowMethods: "GET, POST, PUT, DELETE",  // Permite PUT
+}))
+
 }
+
