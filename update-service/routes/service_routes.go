79653fd59bab05ab2a14e659a066ba1a7f073ae5
AlexMorales <admoralesp@uce.edu.ec>
2025-07-06 19:09:58 -0500

    id mofds

--- update-service/routes/service_routes.go
+++ update-service/routes/service_routes.go
@@ -8,6 +8,6 @@ import (
 func SetupRoutes(app *fiber.App) {
 	api := app.Group("/api/services")
 	api.Put("/:id", controllers.UpdateService)
-	    api.Put("/name/:name", controllers.UpdateServiceByName) // 
+	    api.Put("/id", controllers.UpdateServiceByName) // 
 
 }
